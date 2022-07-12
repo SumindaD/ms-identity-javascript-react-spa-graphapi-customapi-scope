a63842cabf5321b250991269645d43415cc8fe7d
SumindaD <AzureSuminda@AIA.COM>
2022-07-12 07:50:58 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { useMsal, MsalAuthenticationTemplate } from "@azure/msal-react";
+import { InteractionType } from "@azure/msal-browser";
+import { loginRequest, backendAPIRequest } from "./authConfig";
+import { PageLayout } from "./components/PageLayout";
+import { APIData } from "./components/APIData";
+import Button from "react-bootstrap/Button";
+import "./styles/App.css";
+
+/**
+ * Renders name of the signed-in user and a button to retrieve data from an API
+ */
+const AppContent = () => {
+    
+    const { instance, accounts } = useMsal();
+    const [apiData, setApiData] = useState(null);
+
+    function CallAPI() {
+        // Silently acquires an access token which is then attached to a request for API call
+        instance.acquireTokenSilent({
+            ...backendAPIRequest,
+            account: accounts[0]
+        }).then((response) => {
+            console.log(response.accessToken);
+
+            fetch('{Function App API URL}', { 
+                method: 'post', 
+                headers: new Headers({
+                    'Authorization': 'Bearer ' + response.accessToken,
+                    'Accept': 'application/json'
+                })
+            })
+            .then(data => data.json())
+            .then(json => {
+                console.log(json);
+                setApiData(json);
+            });
+
+
+        });
+    }
+
+    function GetGraphInfo() {
+        // Silently acquires an access token which is then attached to a request for API call
+        instance.acquireTokenSilent({
+            ...loginRequest,
+            account: accounts[0]
+        }).then((response) => {
+            console.log(response.accessToken);
+
+            fetch('https://graph.microsoft.com/v1.0/me', { 
+                method: 'get', 
+                headers: new Headers({
+                    'Authorization': 'Bearer ' + response.accessToken,
+                    'Accept': 'application/json'
+                })
+            })
+            .then(data => data.json())
+            .then(json => {
+                console.log(json);
+                setApiData(JSON.stringify(json));
+            });
+
+
+        });
+    }
+
+    return (
+        <>
+            <h5 className="card-title">Welcome {accounts[0].name}</h5>
+            {
+                apiData ? 
+                <APIData apiData={apiData} />
+                :
+                <div>No Data from API. Click Call API!<br/><br/></div>
+            }
+            <Button variant="secondary" onClick={GetGraphInfo}>Get Graph Info</Button>  
+            <Button variant="secondary" onClick={CallAPI}>Call API</Button>
+        </>
+    );
+};
+
+/**
+ * If a user is authenticated the AppContent component above is rendered. Otherwise the content is not rendered.
+ */
+const MainContent = () => { 
+    return (
+        <div className="App">
+            {
+                <MsalAuthenticationTemplate  interactionType={InteractionType.Redirect} authenticationRequest={loginRequest}>
+                    <AppContent />
+                </MsalAuthenticationTemplate>
+            }
+        </div>
+    );
+};
+
+export default function App() {
+    return (
+        <PageLayout>
+            <MainContent />
+        </PageLayout>
+    );
+}
---
+++ src/authConfig.js
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { LogLevel } from "@azure/msal-browser";
+
+/**
+ * Configuration object to be passed to MSAL instance on creation. 
+ * For a full list of MSAL.js configuration parameters, visit:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md 
+ */
+export const msalConfig = {
+    auth: {
+        clientId: "cc18ac91-92f5-4da5-ab53-01e81ec6cc74",
+        authority: "https://login.microsoftonline.com/7f2c1900-9fd4-4b89-91d3-79a649996f0a", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
+        redirectUri: "https://aas-lk01-sea-p-seabreeze-prmcol.azurewebsites.net"
+    },
+    cache: {
+        cacheLocation: "sessionStorage", // This configures where your cache will be stored
+        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+    system: {	
+        loggerOptions: {	
+            loggerCallback: (level, message, containsPii) => {	
+                if (containsPii) {		
+                    return;		
+                }		
+                switch (level) {		
+                    case LogLevel.Error:		
+                        console.error(message);		
+                        return;		
+                    case LogLevel.Info:		
+                        console.info(message);		
+                        return;		
+                    case LogLevel.Verbose:		
+                        console.debug(message);		
+                        return;		
+                    case LogLevel.Warning:		
+                        console.warn(message);		
+                        return;		
+                }	
+            }	
+        }	
+    }
+};
+
+/** 
+ * Scopes you add here will be prompted for user consent during sign-in.
+ * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
+ * For more information about OIDC scopes, visit: 
+ * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
+ */
+export const loginRequest = {
+    scopes: ["User.Read"]
+};
+
+export const backendAPIRequest = {
+    scopes: ["api://cc18ac91-92f5-4da5-ab53-01e81ec6cc74/Invoke"]
+};
